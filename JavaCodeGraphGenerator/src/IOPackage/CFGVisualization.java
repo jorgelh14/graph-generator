9ede5c1ded65ee49737411260162590b23e6fed9
dgonzalez33 <dgonzalez33@miners.utep.edu>
2016-07-29 11:13:47 -0600

    Refactored CFGVisualization class

--- JavaCodeGraphGenerator/src/IOPackage/CFGVisualization.java
+++ JavaCodeGraphGenerator/src/IOPackage/CFGVisualization.java
@@ -1,78 +1,84 @@
 package IOPackage;
 
-
-
 import java.awt.Desktop;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.swing.text.Document;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class CFGVisualization extends IOManager {
 	public CFGVisualization(String fileName){
-		createGraphViz(fileName);
+		createGraphFile(fileName,"pdf");// Types can be gif,dot, fig, pdf, ps, svg, png, and plain.
 	}
-	
-	private void createGraphViz(String filePath){
-		
-			GraphViz graph = new GraphViz();
-			graph.addln(graph.start_graph());
-			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder builder = null;
-			try {
-				builder = factory.newDocumentBuilder();
-			} catch (ParserConfigurationException e) {
-				System.out.println("Couldnt start docment builder");
-			}
-			org.w3c.dom.Document document = null;
-			try {
-				document = builder.parse(new File(filePath));
-			} catch (SAXException | IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			org.w3c.dom.Element rootElement = document.getDocumentElement();
-			
-			 NodeList sourceList = ( rootElement).getElementsByTagName("source");
-			 NodeList targetList = ( rootElement).getElementsByTagName("target");
-			 for(int i=0;i<sourceList.getLength();i++){
-		         NodeList sourceSub = sourceList.item(i).getChildNodes();
-		         String sourceName = sourceSub.item(0).getNodeValue();
-		         NodeList targetSub = targetList.item(i).getChildNodes();
-		         String targetName = targetSub.item(0).getNodeValue();
-	        	 graph.addln("\""+sourceName+"\""+"->"+"\""+targetName+"\"");
-		         if(targetName.startsWith("Join")){
-		        	 graph.addln("\""+targetName+"\""+"[label=\"Join\"]");
-		         }
-		         if(sourceName.startsWith("Join")){
-		        	 graph.addln("\""+sourceName+"\""+"[label=\"Join\"]");
-		         }
-			 }
-		    graph.addln(graph.end_graph());
-			//System.out.println(graph.getDotSource());//testing purposes
-			graph.increaseDpi();   // 106 dpi from java api
 
-			String fileName = filePath.replaceAll(".xml","");
-			String type = "pdf";		//can be changed to dot, fig, pdf, ps, svg, png, and plain. Just replace gif
-			String repesentationType= "dot";// can be changed to neato, fdp, sfdp, twopi, circo.
-			File out = new File(fileName + "." + type);    // change export location here
-			graph.writeGraphToFile( graph.getGraph(graph.getDotSource(), type, repesentationType), out );
-			try {
-				Desktop.getDesktop().open(out);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+	private void createGraphFile(String filePath, String type){
+		GraphViz graph = createGraphViz(filePath);
+		String fileName = filePath.replaceAll(".xml","");
+		String repesentationType= "dot";// can be changed to neato, fdp, sfdp, twopi, circo.
+		File out = new File(fileName + "." + type);    // change export location here
+		graph.writeGraphToFile( graph.getGraph(graph.getDotSource(), type, repesentationType), out );
+		try {
+			Desktop.getDesktop().open(out);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	private GraphViz createGraphViz(String filePath){
+		GraphViz graph = new GraphViz();
+		graph.addln(graph.start_graph());
+		 NodeList sourceList = (getXmlRoot(filePath)).getElementsByTagName("source");
+		 NodeList targetList = (getXmlRoot(filePath)).getElementsByTagName("target");
+		 for(int i=0;i<sourceList.getLength();i++){
+	         NodeList sourceSub = sourceList.item(i).getChildNodes();
+	         String sourceName = sourceSub.item(0).getNodeValue();
+	         NodeList targetSub = targetList.item(i).getChildNodes();
+	         graph = addNodesToGraph(graph,sourceName,targetSub);
+		 }
+	    graph.addln(graph.end_graph());
+		//System.out.println(graph.getDotSource());//testing purposes
+		graph.increaseDpi();   // 106 dpi from java api
+		return graph;
+}
+	
+	private GraphViz addNodesToGraph(GraphViz graph, String sourceName, NodeList targetSub){
+		 if(targetSub.getLength()>0){
+	         String targetName = targetSub.item(0).getNodeValue();
+        	 graph.addln("\""+sourceName+"\""+"->"+"\""+targetName+"\"");
+	         if(targetName.startsWith("Join")){
+	        	 graph.addln("\""+targetName+"\""+"[label=\"Join\"]");
+	         }
+	         if(sourceName.startsWith("Join")){
+	        	 graph.addln("\""+sourceName+"\""+"[label=\"Join\"]");
+	         }
+         }
+         else{
+        	 graph.addln("\""+sourceName+"\";");
+        	 if(sourceName.startsWith("Join")){
+	        	 graph.addln("\""+sourceName+"\""+"[label=\"Join\"]");
+	         }
+         }
+		 return graph;
+	}
+	public org.w3c.dom.Element getXmlRoot(String filePath){
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = null;
+		try {
+			builder = factory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			System.out.println("Couldnt start docment builder");
+		}
+		org.w3c.dom.Document document = null;
+		try {
+			document = builder.parse(new File(filePath));
+		} catch (SAXException | IOException e) {
+			e.printStackTrace();
+		}
+		return document.getDocumentElement();
+	}
+
 	
 }
